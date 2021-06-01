16c426d91022b0e210beec77ac5e1e41d64f2390
yenselleon <76271225+yenselleon@users.noreply.github.com>
2021-06-01 06:58:27 -0400

    funciones terminadas

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const blockquote = document.querySelector('.blockquote');
 const btnNewQuote = document.querySelector('#new-quote');
-
+const tweetQuote = document.getElementById('tweet-quote');
+const backgroundColors = [
+    '#343f56',
+    '#fb9300',
+    '#f54748',
+    '#0a1931',
+    '#1eae98',
+    '#21094e',
+    '#344fa1',
+    '#00adb5',
+    '#02475e',
+    '#ff6701',
+    '#810000',
+    '#206a5d',
+    '#222831',
+];
+let randonNumerUsed;
 
 window.onload = () =>{
-    loading();
+    getNewQuote();
 
 }
 
-const loading = ()=> {
+const startLoader = ()=> {
     btnNewQuote.setAttribute('disabled', 'true');
 
     htmlPureHtmlLoadingBody = `
@@ -17,24 +33,66 @@ const loading = ()=> {
     let createHtmlDivRotate = document.createElement('div');
     createHtmlDivRotate.innerHTML = htmlPureHtmlLoadingBody;
 
-    blockquote.append(createHtmlDivRotate.firstElementChild);
+    return createHtmlDivRotate.firstElementChild;
+}
 
-    setTimeout(()=>{
-        blockquote.innerHTML = '';
+const loading = ()=> {
+    console.log('loader component')
+    let loader = startLoader()
+    blockquote.append(loader);
+
+    loader.classList.add('loading')
+    
+    updateBackgroundColor();
+    let randonNumber = Math.floor(Math.random() * (2000 - 1000)) + 1000;
+    
+    /* setTimeout(()=>{
+        
         getNewQuote();
         btnNewQuote.removeAttribute('disabled');
 
-    }, (Math.random() * (1500 - 3100) + 1500));
+    }, randonNumber); */
 }
 
 
-const getNewQuote = () => {
+const updateBackgroundColor = () => {
+    // let radomBackgroundColorIndex = Math.floor(Math.random() * backgroundColors.length);
+    const randonIndex = randomUnique(backgroundColors.length -1, 2);
 
+    if(randonNumerUsed === undefined) {
+        randonNumerUsed = randonIndex[0];
+    } else{
+        console.log('nuevo numero')
+        randonNumerUsed = randonIndex.filter(n => n !== randonNumerUsed)[0];
+    }
+
+    document.documentElement.style.setProperty(`--base`, `${backgroundColors[randonNumerUsed]}` );
+}
+
+const getNewQuote = () => {
+    
+    btnNewQuote.removeAttribute('disabled');
     fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
         .then(response => response.json())
         .then(data => {
+            blockquote.innerHTML = '';
+            loading();
             let randomNumber = Math.floor(Math.random() * 99);
-            createNewQuote(data.quotes[randomNumber])
+            /* let loader = document.querySelector('.lds-roller');
+            loader.classList.remove('loading')
+            loader.classList.add('hiden') */
+            
+
+            let randonNumber = Math.floor(Math.random() * (2000 - 1000)) + 1000;
+    
+            setTimeout(()=>{
+                blockquote.innerHTML = '';
+                
+                btnNewQuote.removeAttribute('disabled');
+                createNewQuote(data.quotes[randomNumber])
+
+            }, randonNumber);
+            
         }).catch(console.log);
         
 }
@@ -49,14 +107,23 @@ const createNewQuote = (dataQuote) => {
     let createHtmlDiv = document.createElement('div');
     createHtmlDiv.innerHTML = htmlQuoteBody;
 
+    tweetQuote.setAttribute('href',`
+    https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${encodeURIComponent('"'+dataQuote.quote+'" '+'- '+dataQuote.author)}
+    `)
     return blockquote.append(createHtmlDiv);
 }
 
 btnNewQuote.addEventListener('click', ()=> {
 
-    blockquote.innerHTML = '';
-    loading();
-    
+    getNewQuote();
 
 });
 
+
+const randomUnique = (range, count) => {
+    let nums = new Set();
+    while (nums.size < count) {
+        nums.add(Math.floor(Math.random() * (range - 1 + 1) + 1));
+    }
+    return [...nums];
+}
